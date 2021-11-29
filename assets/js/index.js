b04f1c701ba6edad12a3a4fed129a3c501165056
unknown <corona.romero.river@gmail.com>
2021-11-29 00:42:18 -0600

    cambio de cv

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 $().ready(function(){
     gsap.timeline().from(".download",0.5,{x:150},"-=0.5")
     .from(".whats",0.5,{x:150},"-=0.5")
-    .to(".download",0,{delay:1,x:94,transition: "transform 0.8s ease"})
+    .to(".download",0,{delay:1,x:96,transition: "transform 0.8s ease"})
     .to(".whats",0,{x:87,transition: "transform 0.8s ease"});
 
 
